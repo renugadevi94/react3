e7f2ab87a1e59ee7c01d923c2719b8cb42c8a61e
renugadevi94 <srenugadevi1991@gmail.com>
2023-06-25 19:18:42 +0530

    some change

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import './index.css';
+import Dashboard from './components/Dashboard';
+import Buttons from './components/Buttons';
+import TopBar from './components/Topbar';
+import Cards from './components/Cards';
+import Side from './components/Side';
+import {Link,Route,BrowserRouter as Router,Routes} from "react-router-dom";
+
+// following three imports for specific import of an fontawesome icon 
+// for ex: <FontAwesomeIcon icon={faLaughWink} />
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFolder, faLaughWink } from '@fortawesome/free-regular-svg-icons';
+import { faChartArea, faCog, faTable, faTachometerAlt, faWrench } from '@fortawesome/free-solid-svg-icons';
+
+// for using default way of including fontawesome icon 
+// for ex: <i className="fas fa-laugh-wink"></i>
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+// To install fontawesome
+// npm i -S @fortawesome/fontawesome-svg-core @fortawesome/react-fontawesome @fortawesome/free-regular-svg-icons @fortawesome/free-solid-svg-icons @fortawesome/free-brands-svg-icons
 import React from 'react'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Side from './side'
-import Home from './home';
+
+//import {BrowserRouter as Router , Route, Routes } from 'react-router-dom';
+// import {  } from 'react-bootstrap-icons';
 
 function App() {
 
   return (
-    <div className='container-fluid bg-info min-vh-100'>
-      <div className='row'>
-      <div className='col-2 bg-white vh-100'>
-      <Side />
-      </div>
-      <div className='col'>
-        <Home />
-        </div>
-      </div>
-    </div>
+    
+    <div className="App">
+       
+    <Router>
+    
+   <div id="wrapper">
+     <Side />
+     <div id="content-wrapper" className="d-flex flex-column">
+       <div id="content">
+        <TopBar/>
+        
+        
+       
+         <div className="container-fluid">
+         
+             <div>
+              
+             
+             <Link to='/dashboard'>dashboard</Link>
+             <Link to='/tables'>tables</Link>
+             </div>
+             <Routes>
+             <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/components/buttons" element={<Buttons />} />
+            <Route path="/components/cards" element={<Cards />} />
+             </Routes>
+             
+             
+             </div>
+       </div>
+       
+       
+     </div>
+   </div>
+   </Router>
+ </div>   
+    
   )
 }
 
 export default App
-
-
-
-
-
-
